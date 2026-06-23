76555c17d4d13a2f8df9a2f93779c19bb058ffcd
pc-learning-j <teotab08@gmail.com>
2026-06-23 10:55:12 +0200

    Added enhancer tab

--- src/scripts/postbuild.js
+++ src/scripts/postbuild.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
 fs.copyFileSync('styles.css', 'dist/styles.css');
-fs.copyFileSync('tab.html', 'dist/tab.html');
-fs.mkdirSync('dist/lib', { recursive: true });
-fs.copyFileSync('node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js', 'dist/lib/VSS.SDK.min.js');
+fs.mkdirSync('lib', { recursive: true });
+fs.copyFileSync('node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js', 'lib/VSS.SDK.min.js');
